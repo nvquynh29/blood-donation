c8697f135dcb07a1c9141b494f1cd8702ed534ee
VuTan115 <tanboy042001@gmail.com>
2021-11-27 15:58:22 +0700

    fix conflict code FE

--- client/src/components/advise-section/Advise.jsx
+++ client/src/components/advise-section/Advise.jsx
@@ -1,72 +1,74 @@
 import React from 'react'
-import 'font-awesome/css/font-awesome.min.css';
+// import 'font-awesome/css/font-awesome.min.css';
 
 export default function Advise() {
-    return (
-        <div className='adviseSection'>
-            <div className="adviseTitle"><h3 >BẠN CẦN BIẾT</h3></div>
-            <div class="widget-post">
-                <div class="box-item">
-                    <ul class="item">
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Bạn có thể hiến máu ở đâu?</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Khuyến cáo phòng chống dịch bệnh COVID-19</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Hướng dẫn cài đặt và sử dụng ứng…</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Có thể bạn chưa biết về gói xét…</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="box-item">
-                    <ul class="item">
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Một số lưu ý trước và sau hiến…</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Thông báo tuyển sinh các khóa đào tạo…</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Các dịch vụ đào tạo đang triển khai</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Thủ tục khám bệnh và điều trị ngoại…</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="box-item">
-                    <ul class="item">
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Bệnh Thalassemia - hiểu biết, phòng tránh và…</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Tại sao phải xét nghiệm gen Thalassemia?</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Ai có thể bị Hemophilia và bệnh biểu…</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-check-circle"></i>
-                            <a href="">Hướng dẫn lưu giữ máu dây rốn dịch…</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="adviseSection">
+      <div className="adviseTitle">
+        <h3>BẠN CẦN BIẾT</h3>
+      </div>
+      <div class="widget-post">
+        <div class="box-item">
+          <ul class="item">
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Bạn có thể hiến máu ở đâu?</a>
+            </li>
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Khuyến cáo phòng chống dịch bệnh COVID-19</a>
+            </li>
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Hướng dẫn cài đặt và sử dụng ứng…</a>
+            </li>
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Có thể bạn chưa biết về gói xét…</a>
+            </li>
+          </ul>
         </div>
-    )
+        <div class="box-item">
+          <ul class="item">
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Một số lưu ý trước và sau hiến…</a>
+            </li>
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Thông báo tuyển sinh các khóa đào tạo…</a>
+            </li>
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Các dịch vụ đào tạo đang triển khai</a>
+            </li>
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Thủ tục khám bệnh và điều trị ngoại…</a>
+            </li>
+          </ul>
+        </div>
+        <div class="box-item">
+          <ul class="item">
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Bệnh Thalassemia - hiểu biết, phòng tránh và…</a>
+            </li>
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Tại sao phải xét nghiệm gen Thalassemia?</a>
+            </li>
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Ai có thể bị Hemophilia và bệnh biểu…</a>
+            </li>
+            <li>
+              <i class="fa fa-check-circle"></i>
+              <a href="">Hướng dẫn lưu giữ máu dây rốn dịch…</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
