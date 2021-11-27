f334cd99e4b017688c52d36b6cc12824d4f42e6f
Phong Vu UET <vuphonghyuet@gmail.com>
2021-11-27 21:32:26 +0700

    component(requestForBloodForm): create component

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import SideBar from "../../components/sidebar"
+import RequestBloodForm from '../../components/request-blood-form/RequestBloodForm'
 
 
 function index() {
   return (
     <div>
       <SideBar />
+      <RequestBloodForm />
     </div>
+
   );
 }
 
