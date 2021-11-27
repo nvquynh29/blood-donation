c08150e263cf348612473e9f8961e0f218bb0159
Phong Vu UET <vuphonghyuet@gmail.com>
2021-11-28 00:43:02 +0700

    feat(eventInLandingPage): update API to except limit 4 in landingPage + sort By start time + add event list to landingPage

--- client/src/api/event.js
+++ client/src/api/event.js
@@ -1,6 +1,7 @@
 import instance from './axios'
 import { API_URL } from './auth'
 
+export const getEvent = (limit) => instance.get(`${API_URL}/event?limit=${limit}`)
 export const getAllEvent = () => instance.get(`${API_URL}/event`)
 
  
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -7,7 +7,20 @@ import MainLayout from '../layouts/main-layout/Default'
 import ResgiterForm from '../components/reg-form/ResgiterForm'
 import VolunteerForm from '../components/vonlunteer-form/VolunteerForm'
 import Advise from '../components/advise-section/Advise'
+import {getEvent} from '../api/event'
+import { API_URL } from '../api/auth'
+import React, { useState, useEffect } from 'react'
 const Home = () => {
+  const [events, setEvents] = useState([])
+    useEffect(() => {
+        try {
+            getEvent(4).then(res => {
+                setEvents(res.data)
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }, [])
   return (
     <MainLayout>
       <div>
@@ -82,6 +95,51 @@ const Home = () => {
         <div className="register-form">{/* <ResgiterForm /> */}</div>
       </div>
       <VolunteerForm />
+      <div className="mx-auto container">
+            <div className="title mt-10 ml-10 text-red-400 text-4xl font-bold">Các sự kiện sắp diễn ra </div>
+            <div className="grid xl:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-4 px-10 py-10">
+                { events.map((event) => {
+                    const startDate = new Date(event.start_date) 
+                    const endDate = new Date(event.start_date)
+                    endDate.setDate(startDate.getDate() + event.duration)
+                    return ( 
+                <div>
+                    <div class="max-w-md h-full mx-auto bg-white  shadow-md overflow-hidden md:max-w-2xl">
+                        <div class="md:flex flex-col">
+                        <div class="flex-shrink-0">
+                            <img
+                            class=" w-full object-cover h-full"
+                            src={event?.organization_id?.img_path ? `${API_URL}/getFile?img_path=${event?.organization_id?.img_path}` : '../images/slider-1.jpg'}
+                            alt="Man looking at item at a store"
+                            />
+                        </div>
+                        <div class="p-8">
+                            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                            {''}
+                            </div>
+                            <p
+                                class="block mt-1 text-3xl leading-tight font-medium font-Dosis text-black"
+                            >
+                                {event.name}
+                            </p>    
+                            <p class="mt-2 font-Dosis text-gray-500 xl:text-xl lg:text-2xl md:text-base text-base h-auto ">
+                            {event.address}
+                            </p>
+                            <p class="mt-2 font-Dosis text-red-500	 xl:text-xl lg:text-2xl md:text-base text-base h-auto ">
+                            {startDate.toLocaleDateString('pt-PT')}&nbsp;
+                                &#8594;&nbsp;{endDate.toLocaleDateString('pt-PT')}
+                            </p>
+                            <p class="mt-2 font-Dosis text-gray-500	 xl:text-xl lg:text-2xl md:text-base text-base h-auto ">
+                            {event?.organization_id?.name}
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                </div> )
+                }) }
+                                  
+            </div>
+            </div>
       <Advise />
     </MainLayout>
   )
