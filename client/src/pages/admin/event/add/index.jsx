10fe497d4c8097c11cf3844b97a88d3c86189a30
Minh Hieu <hieu0985792248@gmail.com>
2021-12-04 00:17:26 +0700

    admin event pages

--- client/src/pages/admin/event/add/index.jsx
+++ client/src/pages/admin/event/add/index.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Form, Input, Button, notification, DatePicker } from 'antd';
 import router from 'next/router'
 import moment from 'moment'
-import { addEvent } from '../../../../api/event'  
+import { addEvent } from '../../../../api/event'
+import MiniDrawer from '../../../../layouts/trial/MiniDrawer';
 
 export default function AddEventPage() {
-    const onFinish =async (values) => {
+    const onFinish = async (values) => {
         try {
             values.startDate = values.start_date._d.toLocaleDateString('en-CA')
             await addEvent(values)
@@ -15,7 +16,7 @@ export default function AddEventPage() {
                 description: "Đăng ký sự kiện mới thành công!"
             })
             router.push('/admin/event')
-        } catch(error) {
+        } catch (error) {
             console.log(error)
         }
     }
@@ -24,87 +25,83 @@ export default function AddEventPage() {
         console.log('Failed:', errorInfo);
     }
     return (
-        <div className="addEvent">
-            <div className="volunteerForm">
-                <div className="formContain">
-                    <div className="formHeader"><h3 style={{ color: '#ffffff' }}>Đăng ký sự kiện mới</h3></div>
-                    <div className="formMain">
-                        <Form
-                            name="basic"
-                            onFinish={onFinish}
-                            onFinishFailed={onFinishFailed}
-                            autoComplete="off"
-                            >
-                            <Form.Item
-                                name="name"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Nhập tên!',
-                                    },
-                                ]}
-                            >
-                                <Input placeholder='Tên' style={{ height: '40px' }} />
-                            </Form.Item>
+        <MiniDrawer>
+            <div className="addEvent">
+                <div className="title">
+                    Thêm sự kiện
 
-                            
-                                <Form.Item
-                                    name="start_date"
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message: 'Nhập mgày bắt đầu!',
-                                        },
-                                    ]}
-                                >
-                                    <DatePicker placeholder='Ngày bắt đầu' style={{ height: '40px' }} />
-                                </Form.Item>
-                               
-                            
+                </div>
+                <hr />
+                <div className="formContainer">
+                    <Form
+                        name="basic"
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                        layout="vertical"
+                    >
+                        <Form.Item
+                            name="name"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Nhập tên!',
+                                },
+                            ]}
+                            label="Tên"
+                            className="lable"
+                        >
 
-                            <Form.Item
-                                name="address"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Nhập địa chỉ!',
-                                    },
-                                ]}
+                            <Input placeholder='Tên' style={{ height: '40px' }} />
+                        </Form.Item>
+                        <Form.Item
+                            name="start_date"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Nhập mgày bắt đầu!',
+                                },
+                            ]}
+                            className="date"
+                            label="Ngày bắt đầu"
+                        >
 
-                            >
-                                <Input placeholder='Địa chỉ' style={{ height: "100px" }} />
-                            </Form.Item>
-                            <Form.Item
-                                name="duration"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Nhập số ngày diễn ra!',
-                                    },
-                                ]}
+                            <DatePicker placeholder='Ngày bắt đầu' style={{ height: '40px' }} />
+                        </Form.Item>
+                        <Form.Item
+                            name="duration"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Nhập số ngày diễn ra!',
+                                },
+                            ]}
+                            label="Số ngày diễn ra"
+                        >
+                            <Input placeholder='Nhập số ngày diễn ra' style={{ height: "40px" }} />
+                        </Form.Item>
+                        <Form.Item
+                            name="address"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Nhập địa chỉ!',
+                                },
+                            ]}
+                            label="Địa chỉ"
+                        >
 
-                            >
-                                <Input placeholder='Nhập số ngày diễn ra' style={{ height: "100px" }} />
-                            </Form.Item>
+                            <Input placeholder='Địa chỉ' style={{ height: "100px" }} />
+                        </Form.Item>
 
-                            <Form.Item>
-                                <Button type="primary" htmlType="submit"
-                                    className="loginBtn"
-                                    style={{
-                                        height: '40px',
-                                        "font-weight": "400",
-                                        "text-transform": "uppercase",
-                                        "font-size": "18px",
-                                        'letter-spacing': '0.02em',
-                                        "background-color": "#FE3C47"
-                                    }}>
-                                    Đăng ký
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                    </div>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="addEvenBtn">
+                                Thêm
+                            </Button>
+                        </Form.Item>
+                    </Form>
                 </div>
             </div>
-        </div>
+        </MiniDrawer >
     )
 }
