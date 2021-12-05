f06673e2064efbf8856039a6ee9ddbdb4eefd97e
Phong Vu UET <vuphonghyuet@gmail.com>
2021-12-05 13:13:10 +0700

    feat(userStore): add user store to redux + adjust updateUser api

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import volunteerReducer from './volunteerReducer'
+import userReducer from './userReducer'
 
 const rootReducer = combineReducers({
     volunteer: volunteerReducer,
+    user: userReducer,
 })
 
 export default rootReducer
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -7,8 +7,8 @@ const getUser = async (req, res) => {
     const user = await User.findOne({ _id }, {
       name: 1,
       email: 1,
+      role: 1,
     }).exec()
-    console.log(user)
     return res.status(200).json(user)
   } catch (error) {
     return res.status(500).json(error)
@@ -23,15 +23,23 @@ const updateUser = async (req, res) => {
       name, email, currentPassword, newPassword,
     } = req.body
     const user = await User.findOne({ _id })
-    const isValidUser = bcrypt.compareSync(currentPassword, user.password)
-    if (isValidUser) {
-      const password = await bcrypt.hash(newPassword, salt)
+    let isValidUser
+    if (typeof currentPassword !== 'undefined') {
+      isValidUser = bcrypt.compareSync(currentPassword, user.password)
+    }
+    if (isValidUser || typeof currentPassword === 'undefined') {
+      const password = (isValidUser)
+        ? await bcrypt.hash(newPassword, salt) : user.password
       const response = await User.findOneAndUpdate(
         { _id },
         { name, email, password },
         { new: true },
       )
-      return res.status(200).json({ name: response.name, email: response.email })
+      return res.status(200).json({
+        name: response.name,
+        email: response.email,
+        role: response.role,
+      })
     }
     return res.status(403).json({ msg: 'password incorrect' })
   } catch (error) {
