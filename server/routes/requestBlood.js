18e22aff782d672527f9cc6b4302432defdae30e
nvquynh29 <nvquynh29@gmail.com>
2021-12-08 18:19:33 +0700

    feat: request blood crud + fix middleware

--- server/routes/requestBlood.js
+++ server/routes/requestBlood.js
@@ -6,7 +6,9 @@ import isAdmin from '../middleware/AdminMiddleware.js'
 const requestBloodRouter = express.Router()
 
 requestBloodRouter.post('/', RequestBloodController.addRequestBlood)
-requestBloodRouter.get('/', [isAuth, isAdmin], RequestBloodController.getRequests)
+requestBloodRouter.get('/', [isAuth, isAdmin], RequestBloodController.getPendingRequests)
+requestBloodRouter.get('/accepted', [isAuth, isAdmin], RequestBloodController.getAcceptedRequests)
+requestBloodRouter.delete('/:id', [isAuth, isAdmin], RequestBloodController.deleteRequest)
 requestBloodRouter.put('/markAsAccepted/list', [isAuth, isAdmin], RequestBloodController.markAsAccepted)
 
 export default requestBloodRouter
